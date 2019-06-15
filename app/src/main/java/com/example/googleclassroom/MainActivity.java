7864ba3d9abacff2cc4452079771642267b6655d
Erfan Ghobadian <erfanghobadian@yahoo.com>
2019-06-15 19:49:42 +0430

    Adding Some Features Complete UI for MainActivity (Drawer , Drawer Header ,  Add Button , FAB)
    Comment Some Code

--- app/src/main/java/com/example/googleclassroom/MainActivity.java
+++ app/src/main/java/com/example/googleclassroom/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.googleclassroom;
 
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,8 +11,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout mdrawerLayout ;
     private ActionBarDrawerToggle mToggle ;
     private Toolbar mToolbar ;
@@ -22,20 +26,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        // For Toolbar
         mToolbar = findViewById(R.id.nav_action) ;
         setSupportActionBar(mToolbar);
 
+
+        // For Drawer
         mdrawerLayout = findViewById(R.id.drawerLayout);
-        mToggle = new ActionBarDrawerToggle(this,mdrawerLayout,R.string.nav_open,R.string.nav_close) ;
 
 
 
-        mdrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
+        // For Add Buttons to Toolbar
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mdrawerLayout, mToolbar, R.string.nav_open, R.string.nav_close);
+        mdrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+        // For FAB
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,28 +55,70 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
     }
 
+
+
+    // For Opening the Drawer
+    @Override
+    public void onBackPressed() {
+        if (mdrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mdrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    // For Menu in Toolbar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.actionbar , menu);
         return true;
     }
 
+
+    // For Items Selected in Toolbar
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
+        int id  = item.getItemId();
 
-            case R.id.action_add:
-                return true;
+        if (id == R.id.action_add) {
 
-            default:
-                return super.onOptionsItemSelected(item);
+            Toast.makeText(this, "Add Button", Toast.LENGTH_SHORT).show();
 
         }
+        else if (id == R.id.action_settings) {}
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    // For Items Selected in Drawer
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+//        int id = item.getItemId();
+//
+//        if (id == R.id.nav_home) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_tools) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
+
+        mdrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 
