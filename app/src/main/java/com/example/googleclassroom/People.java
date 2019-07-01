fd016a5be9e072aaf64fb3d3ed741da559453ec8
Erfan Ghobadian <erfanghobadian@yahoo.com>
2019-07-01 16:28:14 +0430

    SomeThing

--- app/src/main/java/com/example/googleclassroom/People.java
+++ app/src/main/java/com/example/googleclassroom/People.java
@@ -1,7 +1,11 @@
 package com.example.googleclassroom;
 
 
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,7 +15,15 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.ref.WeakReference;
+import java.net.Socket;
 
 
 /**
@@ -61,6 +73,7 @@ public class People extends Fragment {
         setHasOptionsMenu(true);
 
 
+
         studentbtn = v.findViewById(R.id.studentBTN);
         teacherbtn = v.findViewById(R.id.teacherBTN);
 
@@ -72,7 +85,7 @@ public class People extends Fragment {
 
         rvstudent = v.findViewById(R.id.studentsrv) ;
         LinearLayoutManager llm2 = new LinearLayoutManager(getContext());
-        rvteacher.setLayoutManager(llm2);
+        rvstudent.setLayoutManager(llm2);
 
         intadaper();
 
@@ -80,6 +93,23 @@ public class People extends Fragment {
         teacherbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                LayoutInflater inflater = requireActivity().getLayoutInflater();
+                View vi = inflater.inflate(R.layout.dialog_add_teacher, null);
+                final EditText tusername =  vi.findViewById(R.id.cteachername);
+                builder.setView(vi).setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }) ;
+                AlertDialog dialog = builder.create();
+                dialog.show();
 
             }
         });
@@ -88,6 +118,28 @@ public class People extends Fragment {
         studentbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                LayoutInflater inflater = requireActivity().getLayoutInflater();
+                View vi = inflater.inflate(R.layout.dialog_add_student, null);
+                final EditText studentusername =  vi.findViewById(R.id.cstudent_username);
+                builder.setView(vi).setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        for (User st:myclass.students) {
+                            if (st.username.equals(studentusername.getText().toString())){
+                                Toast.makeText(getContext(), "Student Already in Class", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }) ;
+                AlertDialog dialog = builder.create();
+                dialog.show();
 
             }
         });
@@ -111,10 +163,62 @@ public class People extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id==R.id.pepaction_refresh) {}
+        if (id==R.id.pepaction_refresh) {
+            RefreshPOE refreshPOE = new RefreshPOE(this);
+            refreshPOE.execute("RefreshCLW", user.username, user.password, myclass.code) ;
+        }
         else if (id==R.id.pepaction_about) {}
         else if (id==R.id.pepaction_noti) {}
         return super.onOptionsItemSelected(item);
     }
 
+
+
 }
+
+
+
+class RefreshPOE extends AsyncTask<String , Void , String> {
+    Socket s ;
+    ObjectOutputStream oos ;
+    ObjectInputStream ois ;
+    WeakReference<People> activityReference ;
+    User user ;
+    Class myclass ;
+    RefreshPOE(People context) {
+        activityReference = new WeakReference<>(context);
+    }
+    @Override
+    protected String doInBackground(String... strings) {
+        try {
+            s = new Socket("10.0.2.2" , 8080);
+            oos = new ObjectOutputStream(s.getOutputStream());
+            ois = new ObjectInputStream(s.getInputStream());
+            oos.writeObject(strings);
+            oos.flush();
+
+            user = (User)ois.readObject();
+            myclass = (Class)ois.readObject();
+
+
+
+            oos.close();
+            ois.close();
+            s.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        People activity = activityReference.get();
+        activity.user = user ;
+        activity.myclass = myclass ;
+        activity.intadaper();
+    }
+}
+
--- app/src/main/java/com/example/googleclassroom/STUAdapter.java
+++ app/src/main/java/com/example/googleclassroom/STUAdapter.java
@@ -1,12 +1,22 @@
 package com.example.googleclassroom;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 import java.util.ArrayList;
 
 public class STUAdapter extends RecyclerView.Adapter<STUAdapter.STUViewHolder> {
@@ -18,6 +28,7 @@ public class STUAdapter extends RecyclerView.Adapter<STUAdapter.STUViewHolder> {
         STUViewHolder(View itemView) {
         super(itemView);
             StudentName = itemView.findViewById(R.id.studentname);
+            imageButton = itemView.findViewById(R.id.estudentBTN);
         }
     }
 
@@ -50,6 +61,13 @@ public class STUAdapter extends RecyclerView.Adapter<STUAdapter.STUViewHolder> {
     public void onBindViewHolder( STUViewHolder stuViewHolder, int i) {
         final User student = students.get(i);
         stuViewHolder.StudentName.setText(student.username);
+        System.out.println(student.username);
+        stuViewHolder.imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(v , myclass , student);
+            }
+        });
 
     }
 
@@ -57,4 +75,67 @@ public class STUAdapter extends RecyclerView.Adapter<STUAdapter.STUViewHolder> {
     public int getItemCount() {
         return students.size();
     }
+
+    private void showPopupMenu(final View view , final Class cls , final User student) {
+        // inflate menu
+        final PopupMenu popup = new PopupMenu(view.getContext(),view );
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.estudentmenu, popup.getMenu());
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                int id= menuItem.getItemId();
+                if (id == R.id.remove_student) {
+                    popup.dismiss();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+
+                    builder.setMessage("Remove Student " + student.username + " From Class??").setTitle("Sure ?");
+
+                    builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            new Thread(){
+                                @Override
+                                public void run() {
+                                    super.run();
+                                    try {
+                                        Socket s = new Socket("10.0.2.2" , 8080);
+                                        ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
+                                        ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
+
+                                        String[] a = {"RemoveFromClass" , student.username , student.password , cls.code  };
+                                        oos.writeObject(a);
+                                        oos.flush();
+
+
+
+
+                                        oos.close();
+                                        ois.close();
+                                        s.close();
+
+                                    }catch (Exception e){
+                                        e.printStackTrace();
+                                    }
+                                }
+
+                            }.start();
+                            RefreshPOE refreshPOE = new RefreshPOE(activity);
+                            refreshPOE.execute("RefreshCLW", user.username, user.password, myclass.code) ;
+                        }
+                    });
+                    builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.dismiss();
+                        }
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                return false;
+            }
+        });
+        popup.show();
+    }
+
 }
